737d834a2e25f5752bc8c7d30bcd0f4423016f0d
Ottigan <jmalcans91@gmail.com>
2020-11-27 00:33:03 +0200

    Finalized point #9

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 import React, { useState, Fragment } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
-import { TasksCollection } from '/imports/api/TasksCollection';
+import { TasksCollection } from '/imports/db/TasksCollection';
 import { Task } from './Task';
 import { TaskForm } from './TaskForm';
 import { LoginForm } from './LoginForm';
 
 const toggleChecked = ({ _id, isChecked }) => {
-	Meteor.call('task.setIsChecked', _id, !isChecked);
+	Meteor.call('tasks.setIsChecked', _id, !isChecked);
 };
 
 const deleteTask = ({ _id }) => Meteor.call('tasks.remove', _id);
@@ -23,22 +23,23 @@ export const App = () => {
 
 	const pendingOnlyFilter = { ...hideCompletedFilter, ...userFilter };
 
-	const tasks = useTracker(() => {
-		if (!user) {
-			return [];
+	const { tasks, pendingTasksCount, isLoading } = useTracker(() => {
+		const noDataAvailable = { tasks: [], pendingTasksCount: 0 };
+		if (!Meteor.user()) {
+			return noDataAvailable;
+		}
+		const handler = Meteor.subscribe('tasks');
+
+		if (!handler.ready()) {
+			return { ...noDataAvailable, isLoading: true };
 		}
 
-		return TasksCollection.find(hideCompleted ? pendingOnlyFilter : userFilter, {
+		const tasks = TasksCollection.find(hideCompleted ? pendingOnlyFilter : userFilter, {
 			sort: { createdAt: -1 },
 		}).fetch();
-	});
+		const pendingTasksCount = TasksCollection.find(pendingOnlyFilter).count();
 
-	const pendingTasksCount = useTracker(() => {
-		if (!user) {
-			return 0;
-		}
-
-		return TasksCollection.find(pendingOnlyFilter).count();
+		return { tasks, pendingTasksCount };
 	});
 
 	const pendingTasksTitle = `${pendingTasksCount ? ` (${pendingTasksCount})` : ''}`;
@@ -72,6 +73,8 @@ export const App = () => {
 								{hideCompleted ? 'Show All' : 'Hide Completed'}
 							</button>
 						</div>
+
+						{isLoading && <div className="loading">loading...</div>}
 
 						<ul className="tasks">
 							{tasks.map(task => (
